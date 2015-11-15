9e78a076e5cebb8dacf80d29fb5d62aa4a78d9d3
exphoenix666 <smolenskyk@gmail.com>
2015-11-15 14:17:35 +0200

    add part of entities

---
+++ Solution/Domain/Entities/MatchResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class MatchResult
+    {
+        public List<Player> Team1 { get; set; }
+        public List<Player> Team2 { get; set; }
+        public List<int> GameScores { get; set; } //Team1 did loss game if score less then 0
+        public bool IsTeam1Win 
+        { 
+            get
+            {
+                int winsCount = 0;
+                foreach (var item in GameScores)
+                {
+                    if (item>0)
+                    {
+                        winsCount++;
+                    }
+                    else
+                    {
+                        winsCount--;
+                    }
+                }
+                return winsCount > 0;
+            } 
+        }
+        
+        public MatchResult()
+        {
+            this.GameScores = new List<int>();
+        }
+    }
+}
---
+++ Solution/Domain/Entities/Tournament.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Tournament
+    {
+        public List<Player> Players { get; set; }
+
+        public Tournament()
+        {
+            this.Players = new List<Player>();
+        }
+    }
+}
